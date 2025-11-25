8ed0b4b8b0b8c517dc22255017ba91bfbbf2e076
sohelr-dev <sohe.4273@gmail.com>
2025-11-25 13:12:09 +0600

    add needed dependancy

--- multi-vendor-react-project/src/main.tsx
+++ multi-vendor-react-project/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/'
+// import { createBrowserRouter } from 'react-router-dom'
+
+// const AppRouter = createBrowserRouter
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
